87e2a8449a18302f1314e14f1456de1cb8a04167
Danielroxs <danroxsll@gmail.com>
2025-01-01 20:12:19 -0600

    first commit

---
+++ todo-app-ts/eslint.config.js
@@ -0,0 +1,26 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser
+    },
+    settings: {
+      react: {
+        version: "detect" // Detecta automáticamente la versión de React.
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off", // No requiere que React esté en alcance para JSX.
+      "react/jsx-uses-react": "off",    // Desactiva la regla obsoleta para React en JSX.
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
